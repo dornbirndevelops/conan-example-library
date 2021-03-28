38ccf51b23400620d20c3f784b40751d8d6e01f0
dornbirndevelops <dornbirndevelops@gmail.com>
2021-03-28 13:37:45 +0200

    improve cmake project configuration, add windows dll support, reorganize receipe

--- conanfile.py
+++ conanfile.py
@@ -4,63 +4,77 @@ from conans import ConanFile, CMake
 class DenaLibraryConan(ConanFile):
     name = "dena_library"
     version = "1.0"
-    # The combination of these fields make a package unique
-    settings = "os", "compiler", "build_type", "arch"
-    # Conan creates for the required Conan packages
-    # a find_package script (module style)
-    generators = "cmake_find_package"
-    # If a required Conan package does not have binary package
-    # then it can build it.
-    build_policy = "missing"
+
+    description = "Example showcase on how to develop a C++ library depending on other libraries with Conan Package Manager and CMake"
     url = "https://github.com/dornbirndevelops/conan-example-library"
     license = "feel free to use it"
-    description = "Example showcase on how to develop a C++ library depending on other libraries with Conan Package Manager and CMake"
 
-    # Tools required for building
-    def build_requirements(self):
-        self.build_requires("cmake/3.19.6@")
+    # combination of settings and options create a unique identifier
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    
+    # build the package automatically if requested configuration is not available
+    build_policy = "missing"
+    # use cmakes "find_package" functionality to access the required software packages
+    generators = "cmake_find_package"
+    # prevent copying sources to build folder
+    no_copy_source = True
 
-    # Conan packages needed for building
-    def requirements(self):
-        self.requires("poco/1.10.1@")
-
-    # Copy the source files (relative to conanfile.py)
-    # into the Conan package (self.export_sources_folder)
+    # provide sources within this package
     def export_sources(self):
         self.copy("src/*")
         self.copy("include/*")
         self.copy("CMakeLists.txt")
-        self.copy("conanfile.py")
 
-    def _configure_cmake(self):
+    # manipulate available options based on settings used
+    def config_options(self):
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
+
+    #def configure(self):
+
+    # required software packages during execution
+    def requirements(self):
+        self.requires("poco/1.10.1@")
+
+    #def package_id(self):
+
+    # required software packages during build
+    def build_requirements(self):
+        self.build_requires("cmake/3.19.6@")
+
+    #def system_requirements(self):
+
+    #def build_id(self):
+
+    # provide external source code from outside the package
+    def source(self):
+        pass
+
+    # bring in required software package files needed at runtime
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def _get_configured_cmake(self):
         cmake = CMake(self)
         #cmake.definitions["SOME_DEFINITION"] = "VALUE"
         cmake.configure()
         return cmake
 
-    # How to build the package
+    # compile the binaries of the package
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        self._get_configured_cmake().build()
 
-    # How to create the package
+    # bundle the necessary resources into the package 
     def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-
+        self._get_configured_cmake().install()
+    
+    # provide content information to the future consumer
     def package_info(self):
         self.cpp_info.includedirs = ['include']  # Ordered list of include paths
         self.cpp_info.libs = ['dena_library']  # The libs to link against
-        #self.cpp_info.system_libs = []  # System libs to link against
         self.cpp_info.libdirs = ['lib']  # Directories where libraries can be found
-        #self.cpp_info.resdirs = ['res']  # Directories where resources, data, etc. can be found
-        #self.cpp_info.bindirs = ['bin']  # Directories where executables and shared libs can be found
-        #self.cpp_info.srcdirs = []  # Directories where sources can be found (debugging, reusing sources)
-        #self.cpp_info.build_modules = {}  # Build system utility module files
-        #self.cpp_info.defines = []  # preprocessor definitions
-        #self.cpp_info.cflags = []  # pure C flags
-        #self.cpp_info.cxxflags = []  # C++ compilation flags
-        #self.cpp_info.sharedlinkflags = []  # linker flags
-        #self.cpp_info.exelinkflags = []  # linker flags
-        #self.cpp_info.components  # Dictionary with the different components a package may have
-        #self.cpp_info.requires = None  # List of components from requirements
+        self.cpp_info.bindirs = ['bin']  # Directories where executables and shared libs can be found
